7a0b2e1025ee03b1da9fbe4df154ec9cd0db4ec1
cukaigwe <chido.ukaigwe@sonymusicpub.com>
2023-04-02 18:47:34 +0100

    Home app acts as an SPA, bringing in the other micro-frontend spas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HomeContent from "./components/HomeContent";
+// import HomeContent from "./components/HomeContent";
 import "remixicon/fonts/remixicon.css";
-import Footer from "./Footer";
-import Header from "./Header";
+// import Footer from "./Footer";
+// import Header from "./Header";
+import MainLayout from './MainLayout'
+
 import "./index.scss";
 
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">
-      <HomeContent/>
-    </div>
-    <Footer/>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+// const App = () => (
+//   <div className="text-3xl mx-auto max-w-6xl">
+//     <Header />
+//     <div className="my-10">
+//       <HomeContent/>
+//     </div>
+//     <Footer/>
+//   </div>
+// );
+
+ReactDOM.render(<MainLayout />, document.getElementById("app"));
---
+++ src/MainLayout.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
+import Footer from "home/Footer";
+import Header from "home/Header";
+import "remixicon/fonts/remixicon.css";
+import "./index.scss";
+
+
+import PDPContent from "pdp/PDPContent";
+import HomeContent from "home/HomeContent";
+import CartContent from "cart/CartContent";
+
+export default function MainLayout() {
+
+return (
+  <BrowserRouter>
+      <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+        <Route path="/" element={<HomeContent/>} />
+          <Route path="/product/:id" element={<PDPContent />} />
+          <Route path="/cart" element={<CartContent/>} />
+      </Routes>
+      </div>
+      <Footer />
+    </div>
+  </BrowserRouter>
+  
+);
+    
+}
+
